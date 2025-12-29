54c0b22cf37c514b7d25cf41b87ce925381693ef
namgyeongtae <gotsuebel@gmail.com>
2025-12-29 10:40:17 +0900

    Avoid Obstacles

--- Assets/_Study/02. Scripts/AvoidObstaclesMove.cs
+++ Assets/_Study/02. Scripts/AvoidObstaclesMove.cs
@@ -19,15 +19,42 @@ public class AvoidObstaclesMove : MonoBehaviour
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
             if (Physics.Raycast(ray, out hit, 1000f))
             {
                 targetPoint = hit.point;
             }
         }
+
+        Vector3 dir = (targetPoint - transform.position).normalized;
+
+        dir = GetAvoidanceDirection(dir);
+
+        if (Vector3.Distance(targetPoint, transform.position) < 1f)
+            return;
+        
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+        Quaternion rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, steeringForce * Time.deltaTime);
+    }
+
+    private Vector3 GetAvoidanceDirection(Vector3 dir)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, minDistToAvoid, obstacleMask))
+        {
+            Vector3 hitNormal = hit.normal;
+            hitNormal.y = 0f;
+
+            dir = transform.forward + hitNormal * force; // 플레이어가 바라보는 방향 Vector + hit Normal Vector = 플레이어가 장애물을 피해 이동하는 방향 Vector
+            dir.Normalize();
+        }
+
+        return dir;
     }
 }
