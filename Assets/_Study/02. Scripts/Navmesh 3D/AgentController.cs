8211742fac13704f43d82168b1e32861756ba62c
namgyeongtae <gotsuebel@gmail.com>
2025-12-30 11:51:57 +0900

    Test Navmesh 3D

--- Assets/_Study/02. Scripts/Navmesh 3D/AgentController.cs
+++ Assets/_Study/02. Scripts/Navmesh 3D/AgentController.cs
@@ -1,18 +1,42 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform destination;
+
+    public NavMeshSurface surface;
+
+    public float bakeDistance = 10f;
 
     void Start()
     {
+        RebuildNavMesh();
         agent = GetComponent<NavMeshAgent>();
+
     }
 
     void Update()
     {
-        agent.SetDestination(destination.position);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        var dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+
+        agent.SetDestination(transform.position + dir * 4f);
+
+        float dist = Vector3.Distance(transform.position, surface.transform.position);
+        if (dist > bakeDistance)
+        {
+            RebuildNavMesh();
+        }
+    }
+
+    private void RebuildNavMesh()
+    {
+        surface.transform.position = transform.position;
+        surface.BuildNavMesh();
     }
 }
